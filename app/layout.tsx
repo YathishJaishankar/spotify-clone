c6b1e8d2a876ffe96ddf42d024078115d363443f
Yathish Jaishankar <yathishjaishankar7@gmail.com>
2024-09-04 17:27:27 +0530

    hellsf

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { Inter } from 'next/font/google'; 
+ 
 
 const font = Inter({ subsets: ['latin'] });
 
